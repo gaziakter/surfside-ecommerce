6c10ca468333ed7554c09f9db97d2c7470174254
Gazi Akter <gaziakterpro@gmail.com>
2023-04-28 10:34:06 +0600

    update the cart item and sub total

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,23 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
-{
+{   
+
+    public function increaseQuantity($rowID){
+        $product = Cart::get($rowID);
+        $qty = $product->qty + 1;
+        Cart::update($rowID, $qty);
+    }
+
+    public function decreaseQuantity($rowID){
+        $product = Cart::get($rowID);
+        $qty = $product->qty - 1;
+        Cart::update($rowID, $qty);
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
